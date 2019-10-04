c48cf08fe5fdb2218ed5871b6d74abacf291fc05
MR.ROBOT <kuzivladimir@rambler.ru>
2019-10-04 21:24:53 +0300

    Добавлены урл категориям

--- site2/bboard/urls.py
+++ site2/bboard/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 
 from django.urls import path
 
-from .views import index
+from .views import index, by_rubric
 
 urlpatterns = [
-	
+	path('<int:rubric_id>/', by_rubric),
     path('',index),
 ]
--- site2/bboard/views.py
+++ site2/bboard/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
 from .models import Bb
+from .models import Rubric
+
+def by_rubric(request, rubric_id):
+	bbs = Bb.objects.filter(rubric = rubric_id)
+	rubrics = Rubric.objects.all()
+	current_rubric = Rubric.objects.get(pk = rubric_id)
+	context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+	return render(request, "by_rubric.html", context)
 
 
 def index(request):
 	
-	bbs = Bb.objects.order_by('-published')
-	return render(request, 'bboard.html', {'bbs':bbs})
+	bbs = Bb.objects.all()
+	rubrics = Rubric.objects.all()
+	context = {'bbs': bbs, 'rubrics': rubrics}
+	return render(request, 'bboard.html', context)
 
 # Create your views here.
